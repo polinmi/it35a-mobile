3e05d9bf89a5f4976bd1251b4a0ad740dfff58c0
polinmi <jeonmay7@gmail.com>
2026-04-06 08:13:41 +0800

    Added Initital Login button

---
+++ src/pages/Login.tsx
@@ -0,0 +1,33 @@
+import { IonButton, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonButtons, useIonRouter } from "@ionic/react";
+
+const Login: React.FC = () => {
+ const navigation = useIonRouter();
+
+      const doLogin = () => {
+    navigation.push('/app', 'forward', 'replace')
+ }
+   return(
+    <IonPage>
+        <IonHeader>
+            <IonToolbar>
+                <IonButtons>
+                    <IonMenuButton>
+
+                    </IonMenuButton>
+                </IonButtons>
+                <IonTitle>Login </IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen >
+            <IonButton expand= "full" onClick={() => doLogin ()}>
+                Login
+            </IonButton>
+
+        </IonContent>
+    </IonPage>
+
+   );
+
+}
+
+export default Login;
